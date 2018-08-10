9e3c90d3735efb84f491b3c7f265916f01198434
htennent <harrytennent12@gmail.com>
2018-08-10 23:51:31 +0100

    Adding sorting algorithm implementations Added quicksort/insertion sort that extend from a sorter interface

--- src/linkedList/Node.java
+++ src/linkedList/Node.java
@@ -1,9 +1,17 @@
 package linkedList;
 
 public class Node<T> {
+	
+	public T data;
+	public Node<T> next;
 
-	public Node() {
-		// TODO Auto-generated constructor stub
+	public Node(T data, Node<T> next) {
+		super();
+		if(data == null) {
+			throw new IllegalArgumentException("Data must not be null");
+		}
+		this.data = data;
+		this.next = next;
 	}
 
 }
